2dd9ce783e2744f5d104d68f03eb471f19746134
Benjamin <benraskin92@gmail.com>
2015-08-17 06:55:21 -0400

    About and contact

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'static_pages#home'
+  get '/contact' => 'static_pages#contact'
+  get '/about' => 'static_pages#about'
 
   devise_scope :user do
     get "/login" => "devise/sessions#new"
